24df330ac8707c524fc265265287ce18d5003bda
idanok <ida.nokland@hotmail.com>
2025-09-30 13:02:42 +0200

    Add ESLint and Prettier configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,63 @@
 import js from "@eslint/js";
-import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  // JavaScript & React
+  {
+    files: ["**/*.{js,jsx}"],
+    ignores: ["node_modules/**", "*.css", "*.html", "*.md"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: { js, react: pluginReact },
+    rules: {
+      "react/display-name": "off",
+      "react/prop-types": "off", // optional
+    },
+  },
+
+  // JSON
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    rules: {},
+  },
+
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    rules: {},
+  },
+
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    rules: {},
+  },
+
+  // Markdown
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    rules: {},
+  },
+
+  // CSS
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    rules: {},
+  },
 ]);
